afe4036c10b577b4e15405b5fd47e29d0ac5e4b0
projectbima <72653986+projectbima@users.noreply.github.com>
2020-11-08 11:23:18 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -28,16 +28,16 @@ define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settin
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'seruyanku' );
+define( 'DB_NAME', 'sql12375240' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'sql12375240' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'x2FFqlvwnx' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql12.freesqldatabase.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
